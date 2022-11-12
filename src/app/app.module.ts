13b7c9c09014a28926e96f0b35a0b0d5d4f4dac4
sanyprikol <sanyakolomijchuk07@gmail.com>
2022-11-12 20:50:15 +0200

    HomeWorkAngular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,27 @@ import { PostsComponent } from './components/posts/posts.component';
 import { PostComponent } from './components/post/post.component';
 import { CommentsComponent } from './components/comments/comments.component';
 import { CommentComponent } from './components/comment/comment.component';
+import {RouterModule, Routes} from "@angular/router";
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { CommentDetailsComponent } from './components/comment-details/comment-details.component';
+
+const routes: Routes = [
+  {path: '', component:MainLayoutComponent, children:[
+      {path: '', redirectTo: 'users', pathMatch:'full'},
+      {path: 'users', component:UsersComponent, children:[
+          {path:':id', component: UserDetailsComponent}
+        ]},
+      {path: 'posts', component:PostsComponent, children:[
+          {path: ':id', component: PostDetailsComponent}
+        ]},
+      {path: 'comments', component:CommentsComponent, children:[
+          {path: ':id', component: CommentDetailsComponent}
+        ]}
+    ]},
+]
 
 @NgModule({
   declarations: [
@@ -18,11 +39,17 @@ import { CommentComponent } from './components/comment/comment.component';
     PostsComponent,
     PostComponent,
     CommentsComponent,
-    CommentComponent
+    CommentComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    UserDetailsComponent,
+    PostDetailsComponent,
+    CommentDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
